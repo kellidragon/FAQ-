bee33678a8900b637b8d1a1a2e0d6451710f0c3e
kellidragon <dragonkelli@gmail.com>
2019-08-29 10:45:28 -0700

    completed html, css, js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// Toggle Collapse
+$('.faq li .question').click(function () {
+    $(this).find('.plus-minus-toggle').toggleClass('collapsed');
+    $(this).parent().toggleClass('active');
+  });
+  
+  
